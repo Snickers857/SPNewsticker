c9d63f5372995470c5c65a6f217b1d6137a81117
Hannes Magnusson <h_magnusson92@hotmail.com>
2020-10-12 15:56:58 +0200

    Newsticker als Webpart

---
+++ SPNewsticker/Pages/Newsticker1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page language="C#" Inherits="Microsoft.SharePoint.WebPartPages.WebPartPage, Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register Tagprefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register Tagprefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register Tagprefix="WebPartPages" Namespace="Microsoft.SharePoint.WebPartPages" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+
+<WebPartPages:AllowFraming ID="AllowFraming" runat="server" />
+
+<html>
+<head>
+    <title></title>
+
+    <script type="text/javascript" src="../Scripts/jquery-1.9.1.min.js"></script>
+    <script type="text/javascript" src="/_layouts/15/MicrosoftAjax.js"></script>
+    <script type="text/javascript" src="/_layouts/15/sp.runtime.js"></script>
+    <script type="text/javascript" src="/_layouts/15/sp.js"></script>
+
+    <script src="../Scripts/jquery.li-scroller.1.0.js"></script>
+    <script src="../Scripts/Newsticker.js"></script>
+    <link href="../Content/li-scroller.css" rel="stylesheet" />
+    <script type="text/javascript">
+        // Festlegen des Formats für die Clientwebpart-Seite, sodass es mit dem Hostweb konsistent ist.
+     
+    </script>
+</head>
+<body>
+     <div>
+        <ul id="ticker01"></ul>       
+    </div>
+</body>
+</html>
---
+++ SPNewsticker/Scripts/Newsticker.js
@@ -0,0 +1,74 @@
+﻿'use strict';
+
+var hostUrl = "";
+var allNews;
+var context = SP.ClientContext.get_current();
+
+if (document.URL.indexOf('?') != -1) {
+        var params = document.URL.split('?')[1].split('&');
+        for (var i = 0; i < params.length; i++) {
+            var p = decodeURIComponent(params[i]);
+            if (/^SPHostUrl=/i.test(p)) {
+                hostUrl = p.split('=')[1];
+                document.write('<link rel="stylesheet" href="' + hostUrl + '/_layouts/15/defaultcss.ashx" />');
+                break;
+            }
+        }
+    }
+    if (hostUrl == '') {
+        document.write('<link rel="stylesheet" href="/_layouts/15/1033/styles/themable/corev15.css" />');
+    }
+
+
+
+$(document).ready(function () {
+    var hostweburl = decodeURIComponent(getQueryStringParamenter("SPHostUrl"));
+    var hostcontext = new SP.AppContextSite(context, hostweburl);
+    var web = hostcontext.get_web();
+
+
+    var list = web.get_lists().getByTitle("News");
+    var camlString =
+        "<View><ViewFields>" +
+        "<FieldRef Name='Title' />" +
+        "<FieldRef Name='Nachricht' />" +
+        "</ViewFields></View>";
+
+    var camlQuery = new SP.CamlQuery();
+    camlQuery.set_viewXml(camlString);
+    allNews = list.getItems(camlQuery);
+
+    context.load(allNews, "Include(Title, Nachricht)");
+    context.executeQueryAsync(onQuerySucceeded, onQueryFailed);
+});
+
+function onQuerySucceeded() {
+    var newsHTML = "";
+    var enumerator = allNews.getEnumerator();
+
+    while (enumerator.moveNext()) {
+        var announcment = enumerator.get_current();
+        newsHTML = newsHTML + "<li><span> +++ " + announcment.get_item("Title") + " : </span>" + announcment.get_item("Nachricht") + "</li>";
+    }
+    $("#ticker01")[0].innerHTML = newsHTML;
+    $("ul#ticker01").liScroll({ travelocity: "0.15" });
+   
+};
+
+function onQueryFailed(sender, args) {
+    alert('Fehler:' + args.get_message() + '/n' + args.get_stackTrace());
+} 
+
+function getQueryStringParamenter(paramToRetrieve) {
+    var params;
+    var strParams;
+
+    params = document.URL.split("?")[1].split("&");
+    strParams = "";
+
+    for (var i = 0; i < params.length; i = i + 1) {
+        var singleParam = params[i].split("=");
+        if (singleParam[0] == paramToRetrieve)
+            return singleParam[1];
+    } 
+}
